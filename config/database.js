3f101117fa8d75c54dad488a60b9406ec8b5d12d
Jose Tubac <jos96tri@hotmail.com>
2025-03-03 21:51:35 -0600

    Semana1 Proyecto Habitos Atomicos 14010289

---
+++ config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => { 
+    console.log('Database is connected');
+}).catch(err => {
+    console.log('Error connecting to the database')
+    console.log(err);
+});
+
+module.exports = mongoose;
+
